a3f3c0dbba7934a2876c5d38fb5ac01ceeff8752
Vivek Purohit <124659941+vpurohit0000@users.noreply.github.com>
2024-07-17 23:40:33 +0530

    Add files via upload
    Day1

---
+++ CrackYourInternship/Array/maxProfit.java
@@ -0,0 +1,18 @@
+class maxProfit {
+ public static int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;}}
